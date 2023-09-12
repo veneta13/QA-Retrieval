1cfcfa9945dc4cefe0bdf7f48c7c788dd4b7d708
Veneta <venkireva80@gmail.com>
2023-09-13 00:30:35 +0300

    Add function to get answer with DistilBert

---
+++ get_answer.py
@@ -0,0 +1,17 @@
+import numpy as np
+from transformers import TFAutoModelForQuestionAnswering, AutoTokenizer
+
+checkpoint = 'distilbert-base-uncased-distilled-squad'
+model = TFAutoModelForQuestionAnswering.from_pretrained(checkpoint)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+def get_answer(context, question):
+    inputs = tokenizer([context], [question], return_tensors="np")
+    outputs = model(inputs)
+
+    start_position = np.argmax(outputs.start_logits[0])
+    end_position = np.argmax(outputs.end_logits[0])
+    answer = inputs["input_ids"][0, start_position: end_position + 1]
+
+    return tokenizer.decode(answer)
