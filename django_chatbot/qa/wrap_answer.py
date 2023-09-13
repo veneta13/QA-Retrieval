65be9a26428eb6d1249f1df85bf1f279c30bbb27
Veneta <venkireva80@gmail.com>
2023-09-13 22:42:40 +0300

    Add GPT 3.5 wrapper

--- django_chatbot/qa/wrap_answer.py
+++ django_chatbot/qa/wrap_answer.py
@@ -1,17 +1,18 @@
 from langchain.schema import (
-    AIMessage,
     HumanMessage,
     SystemMessage
 )
 from langchain.chat_models import ChatOpenAI
 
 
-if __name__ == '__main__':
+def format_answer(question, answer):
     chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.3)
+    message = f'Question:{question} Answer: {answer}'
+
     messages = [
         SystemMessage(content="You format answers to questions"),
-        HumanMessage(content="Question: what i sthe temperature outside? answer: 20 DEGREES")
+        HumanMessage(content=message)
     ]
-    response = chat(messages)
 
-    print(response.content, end='\n')
+    response = chat(messages)
+    return response.content
