53a47e9e1a79725a86b09e2969a68aa6bfec4849
Veneta <venkireva80@gmail.com>
2023-09-13 19:55:02 +0300

    Remove comments

--- django_chatbot/qa/send_user.py
+++ django_chatbot/qa/send_user.py
@@ -9,4 +9,6 @@ def send_user(query):
     paragraphs = get_paragraphs(wiki_link)
     top_paragraph = get_top_paragraph(paragraphs, query)
     answer = get_answer(top_paragraph, query)
+    if len(answer) < 1:
+        return "I can't find this information.", ""
     return answer, top_paragraph
