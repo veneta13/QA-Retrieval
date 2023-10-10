06bcb7e79e173a7109665f4b00a6800fa88acc3d
Veneta <venkireva80@gmail.com>
2023-10-10 10:53:02 +0300

    Use vector database instead of BM25

--- README.md
+++ README.md
@@ -8,7 +8,8 @@
 
 - User submits a question.
 - The system selects the most relevant Wikipedia article.
-- It ranks the paragraphs within the article using Okapi BM25.
+- It ranks the paragraphs within the article using a semantic search 
+with the HyperDB vector database.
 - The top-ranked paragraph serves as the context for the DistilBERT model.
 - The system receives the response from DistilBERT.
 - Finally, it generates a coherent sentence using Chat GPT and sends it back to the user.
--- qa/rank_paragraphs.py
+++ qa/rank_paragraphs.py
@@ -1,27 +1,6 @@
-import string
-
-from rank_bm25 import BM25Okapi
-
-
-def tokenize_paragraphs(paragraphs):
-    translator = str.maketrans('', '', string.punctuation)
-    paragraphs = [current.translate(translator).lower()
-                  for current
-                  in paragraphs]
-    paragraphs = [current.split(' ') for current in paragraphs]
-    return paragraphs
-
-
-def tokenize_query(query):
-    translator = str.maketrans('', '', string.punctuation)
-    query = query.translate(translator).lower().split()
-    return query
-
+from hyperdb import HyperDB
 
 def get_top_paragraph(paragraphs, query):
-    tokenized_paragraphs = tokenize_paragraphs(paragraphs)
-    tokenized_query = tokenize_query(query)
-
-    bm25 = BM25Okapi(tokenized_paragraphs)
-
-    return bm25.get_top_n(tokenized_query, paragraphs, n=1)[0]
+    database = HyperDB(paragraphs)
+    result = database.query(query, top_k=1)[0][0]
+    return result
